87100016772215cc5dc4e321cdd316488d5579f8
ozline <ozlinex@outlook.com>
2023-12-08 05:21:43 +0800

    feat: improve Docker script toolchain

---
+++ deploy/restart-service.sh
@@ -0,0 +1,65 @@
+#!/bin/bash
+
+# This script file will automatically pull the latest image (if available), exit, delete the original image and container, and finally launch a new container.
+# Usually, we only need to modify the initial few configurations, and the rest of the content does not need to be changed.
+
+IMAGE_NAME="registry.cn-hangzhou.aliyuncs.com/west2-online/fzuhelper-server:latest"
+CONTAINER_NAME_PERFIX="fzuhelper"
+NET_MODE="host"
+
+DIR=$(cd $(dirname $0); pwd)
+CONFIG_PATH=$DIR/config
+CONTAINER_CONFIG_PATH=/app/config
+
+SERVICE_TO_START=${1:-all} # default start all
+
+SERVICES=(template)
+
+remove_container() {
+    container_status=$(docker inspect -f '{{.State.Status}}' "$1")
+    if [ "$container_status" == "running" ]; then
+        echo "Stopping container $1..."
+        docker stop "$1"
+    elif [ "$container_status" == "paused" ]; then
+        echo "Unpausing and then stopping container $1..."
+        docker unpause "$1"
+        docker stop "$1"
+    fi
+    echo "Remove container $1..."
+    docker rm "$1"
+}
+
+start_container() {
+    echo "Starting container for $1..."
+    docker run -d --name "$CONTAINER_NAME_PERFIX-$1" \
+    -e service=$1 \
+    --net=$NET_MODE \
+    -v $CONFIG_PATH:$CONTAINER_CONFIG_PATH \
+    "$IMAGE_NAME"
+}
+
+containers_to_stop=$(docker ps -aq --filter "ancestor=$IMAGE_NAME")
+if [ "$SERVICE_TO_START" == "all" ]; then
+    for container_id in $containers_to_stop; do
+        remove_container $container_id
+    done
+else
+    for container_id in $containers_to_stop; do
+        container_id=$(docker inspect -f '{{.Name}}' "$container_id")
+        if [ "$container_id" != "/$CONTAINER_NAME_PERFIX-$SERVICE_TO_START" ]; then
+            continue
+        fi
+        remove_container $container_id
+    done
+fi
+
+echo "Pulling the latest image..."
+docker pull "$IMAGE_NAME"
+
+if [ "$SERVICE_TO_START" == "all" ]; then
+    for service in "${SERVICES[@]}"; do
+        start_container $service
+    done
+else
+    start_container $SERVICE_TO_START
+fi
