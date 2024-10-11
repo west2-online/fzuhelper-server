8ca06bf7ba8a567b461932a2282db0c215668658
SchwarzSail <119314702+SchwarzSail@users.noreply.github.com>
2024-10-11 20:48:10 +0800

    accelerate empty classroom crawling

--- cmd/classroom/dal/cache/schedule.go
+++ cmd/classroom/dal/cache/schedule.go
@@ -22,6 +22,8 @@ import (
 	"strconv"
 	"time"
 
+	"github.com/west2-online/fzuhelper-server/pkg/utils"
+
 	"golang.org/x/sync/errgroup"
 
 	"github.com/west2-online/fzuhelper-server/config"
@@ -33,11 +35,6 @@ import (
 // ScheduledGetClassrooms 定期获取空教室信息
 func ScheduledGetClassrooms() error {
 	ctx := context.Background()
-	// 定义jwch的stu客户端
-	stu := jwch.NewStudent().WithUser(config.DefaultUser.Account, config.DefaultUser.Password)
-	// 登录，id和cookies会自动保存在client中
-	_ = stu.Login()
-
 	var dates []string
 	currentTime := time.Now()
 	// 设定一周时间
@@ -51,50 +48,61 @@ func ScheduledGetClassrooms() error {
 
 	// 对每个日期启动一个 goroutine
 	for _, date := range dates {
-		date := date
-		eg.Go(func() error {
-			for _, campus := range constants.CampusArray {
-				for startTime := 1; startTime <= 11; startTime++ {
-					for endTime := startTime; endTime <= 11; endTime++ {
-						args := jwch.EmptyRoomReq{
-							Campus: campus,
-							Time:   date,
-							Start:  strconv.Itoa(startTime),
-							End:    strconv.Itoa(endTime),
-						}
-						var res []string
-						var err error
-						// 从jwch获取空教室信息
-						switch campus {
-						case "旗山校区":
-							res, err = stu.GetQiShanEmptyRoom(args)
-						default:
-							res, err = stu.GetEmptyRoom(args)
-						}
-						if err != nil {
-							return fmt.Errorf("ScheduledGetClassrooms: failed to get empty room info: %w", err)
-						}
-						// 收集结果并缓存
-						switch campus {
-						case "厦门工艺美院":
-							err = SetXiaMenEmptyRoomCache(ctx, date, args.Start, args.End, res)
-							if err != nil {
-								return fmt.Errorf("ScheduledGetClassrooms: failed to set xiamen empty room cache: %w", err)
+		// 将 date 作为参数传递给 goroutine
+		eg.Go(func(date string) func() error {
+			return func() error {
+				// 创建新的 stu 对象，每个 goroutine 都有自己的 client
+				// 定义 jwch 的 stu 客户端
+				stu := jwch.NewStudent().WithUser(config.DefaultUser.Account, config.DefaultUser.Password)
+				// 登录，id 和 cookies 会自动保存在 client 中
+				// 如果登录失败，重试
+				err := utils.RetryLogin(stu)
+				if err != nil {
+					return fmt.Errorf("ScheduledGetClassrooms: failed to login: %w", err)
+				}
+				for _, campus := range constants.CampusArray {
+					for startTime := 1; startTime <= 11; startTime++ {
+						for endTime := startTime; endTime <= 11; endTime++ {
+							args := jwch.EmptyRoomReq{
+								Campus: campus,
+								Time:   date, // 使用传递进来的 date 参数
+								Start:  strconv.Itoa(startTime),
+								End:    strconv.Itoa(endTime),
 							}
-						default:
-							key := fmt.Sprintf("%s.%s.%s.%s", args.Time, args.Campus, args.Start, args.End)
-							err = SetEmptyRoomCache(ctx, key, res)
+							var res []string
+							var err error
+							// 从 jwch 获取空教室信息
+							switch campus {
+							case "旗山校区":
+								res, err = stu.GetQiShanEmptyRoom(args)
+							default:
+								res, err = stu.GetEmptyRoom(args)
+							}
 							if err != nil {
-								return fmt.Errorf("ScheduledGetClassrooms: failed to set empty room cache: %w", err)
+								return fmt.Errorf("ScheduledGetClassrooms: failed to get empty room info: %w", err)
+							}
+							// 收集结果并缓存
+							switch campus {
+							case "厦门工艺美院":
+								err = SetXiaMenEmptyRoomCache(ctx, date, args.Start, args.End, res)
+								if err != nil {
+									return fmt.Errorf("ScheduledGetClassrooms: failed to set xiamen empty room cache: %w", err)
+								}
+							default:
+								key := fmt.Sprintf("%s.%s.%s.%s", args.Time, args.Campus, args.Start, args.End)
+								err = SetEmptyRoomCache(ctx, key, res)
+								if err != nil {
+									return fmt.Errorf("ScheduledGetClassrooms: failed to set empty room cache: %w", err)
+								}
 							}
 						}
 					}
-				}
 
-				logger.Infof("ScheduledGetClassrooms: complete all tasks of campus %s in the day %s", campus, date)
+					logger.Infof("ScheduledGetClassrooms: complete all tasks of campus %s in the day %s", campus, date)
+				}
+				return nil
 			}
-			return nil
-		})
+		}(date))
 	}
 
 	// 等待所有 goroutine 完成，并收集错误
