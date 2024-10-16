16ed322a76da1de29960ca4b32c235b0dfda56e3
Baoshuo <i@baoshuo.ren>
2024-10-16 23:49:55 +0800

    init: course

---
+++ idl/course.thrift
@@ -0,0 +1,15 @@
+namespace go course
+include "model.thrift"
+
+struct CourseListRequest {
+    1: required string term
+}
+
+struct CourseListResponse {
+    1: required model.BaseResp base
+    2: required list<model.Course> data
+}
+
+service CourseService {
+    CourseListResponse GetCourseList(1: CourseListRequest req)
+}
