c92db5d15b8646b7fba32dfb606f3d96be0a73d2
SchwarzSail <119314702+SchwarzSail@users.noreply.github.com>
2024-10-18 19:42:15 +0800

    abstract the init of rpc client

--- pkg/client/rpc.go
+++ pkg/client/rpc.go
@@ -20,43 +20,38 @@ import (
 	"errors"
 	"fmt"
 
+	"github.com/west2-online/fzuhelper-server/kitex_gen/classroom/classroomservice"
+	"github.com/west2-online/fzuhelper-server/kitex_gen/user/userservice"
+
 	"github.com/cloudwego/kitex/client"
 	etcd "github.com/kitex-contrib/registry-etcd"
 
 	"github.com/west2-online/fzuhelper-server/config"
-	"github.com/west2-online/fzuhelper-server/kitex_gen/classroom/classroomservice"
-	"github.com/west2-online/fzuhelper-server/kitex_gen/user/userservice"
 	"github.com/west2-online/fzuhelper-server/pkg/constants"
 )
 
-// 定义一系列的RPC客户端
-
-func InitUserRPC() (*userservice.Client, error) {
+// 通用的RPC客户端初始化函数
+func initRPCClient[T any](serviceName string, newClientFunc func(string, ...client.Option) (T, error)) (*T, error) {
 	if config.Etcd == nil || config.Etcd.Addr == "" {
 		return nil, errors.New("config.Etcd.Addr is nil")
 	}
+	// 初始化Etcd Resolver
 	r, err := etcd.NewEtcdResolver([]string{config.Etcd.Addr})
 	if err != nil {
-		return nil, fmt.Errorf("InitUserRPC etcd.NewEtcdResolver failed: %w", err)
+		return nil, fmt.Errorf("initRPCClient etcd.NewEtcdResolver failed: %w", err)
 	}
-	client, err := userservice.NewClient(constants.UserServiceName, client.WithResolver(r), client.WithMuxConnection(constants.MuxConnection))
+	// 初始化具体的RPC客户端
+	client, err := newClientFunc(serviceName, client.WithResolver(r), client.WithMuxConnection(constants.MuxConnection))
 	if err != nil {
-		return nil, fmt.Errorf("InitUserRPC NewClient failed: %w", err)
+		return nil, fmt.Errorf("initRPCClient NewClient failed: %w", err)
 	}
 	return &client, nil
 }
 
+func InitUserRPC() (*userservice.Client, error) {
+	return initRPCClient(constants.UserServiceName, userservice.NewClient)
+}
+
 func InitClassroomRPC() (*classroomservice.Client, error) {
-	if config.Etcd == nil || config.Etcd.Addr == "" {
-		return nil, errors.New("config.Etcd.Addr is nil")
-	}
-	r, err := etcd.NewEtcdResolver([]string{config.Etcd.Addr})
-	if err != nil {
-		return nil, fmt.Errorf("InitClassroomRPC etcd.NewEtcdResolver failed: %w", err)
-	}
-	client, err := classroomservice.NewClient(constants.ClassroomServiceName, client.WithResolver(r), client.WithMuxConnection(constants.MuxConnection))
-	if err != nil {
-		return nil, fmt.Errorf("InitClassroomRPC NewClient failed: %w", err)
-	}
-	return &client, nil
+	return initRPCClient(constants.ClassroomServiceName, classroomservice.NewClient)
 }
