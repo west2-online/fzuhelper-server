1cf49ce7ad4b9bdfa817eb7b5802c15cf15c980b
FantasyRL <1845983502@qq.com>
2024-10-22 11:17:38 +0800

    fix:a warning line

--- cmd/api/biz/handler/api/launch_screen_service.go
+++ cmd/api/biz/handler/api/launch_screen_service.go
@@ -272,7 +272,7 @@ func MobileGetImage(ctx context.Context, c *app.RequestContext) {
 
 	resp := new(api.MobileGetImageResponse)
 
-	respImageList, cnt, err := rpc.MobileGetImageRPC(ctx, &launch_screen.MobileGetImageRequest{
+	respImageList, _, err := rpc.MobileGetImageRPC(ctx, &launch_screen.MobileGetImageRequest{
 		SType:     req.Type,
 		StudentId: req.StudentID,
 		College:   req.College,
@@ -282,7 +282,6 @@ func MobileGetImage(ctx context.Context, c *app.RequestContext) {
 		pack.RespError(c, err)
 		return
 	}
-	resp.Count = cnt
 	resp.PictureList = pack.BuildLaunchScreenList(respImageList)
 
 	pack.RespList(c, resp.PictureList)
