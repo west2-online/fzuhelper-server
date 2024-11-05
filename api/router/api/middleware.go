f07e9751a015b6b4217516d60796b1c7be13c250
SchwarzSail <119314702+SchwarzSail@users.noreply.github.com>
2024-11-05 21:48:51 +0800

    add calling middleware in academic (#80)

--- api/router/api/middleware.go
+++ api/router/api/middleware.go
@@ -152,7 +152,9 @@ func _mobilegetimageMw() []app.HandlerFunc {
 
 func _academicMw() []app.HandlerFunc {
 	// your code...
-	return nil
+	return []app.HandlerFunc{
+		middleware.GetHeaderParams(),
+	}
 }
 
 func _getcreditMw() []app.HandlerFunc {
