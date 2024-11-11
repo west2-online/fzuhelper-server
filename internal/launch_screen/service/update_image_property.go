141b310a632ea9cbe01037e21afd2b624eb49991
FantasyRL <1845983502@qq.com>
2024-11-11 18:44:02 +0800

    fix:timestamp bug

--- internal/launch_screen/service/update_image_property.go
+++ internal/launch_screen/service/update_image_property.go
@@ -21,7 +21,6 @@ import (
 	"time"
 
 	"github.com/west2-online/fzuhelper-server/kitex_gen/launch_screen"
-	"github.com/west2-online/fzuhelper-server/pkg/constants"
 	"github.com/west2-online/fzuhelper-server/pkg/db/model"
 )
 
@@ -36,8 +35,8 @@ func (s *LaunchScreenService) UpdateImageProperty(req *launch_screen.ChangeImage
 	origin.Href = *req.Href
 	origin.Frequency = req.Frequency
 	origin.Text = req.Text
-	origin.StartAt = time.Unix(req.StartAt, 0).Add(constants.TimeZoneOffset * time.Hour)
-	origin.EndAt = time.Unix(req.EndAt, 0).Add(constants.TimeZoneOffset * time.Hour)
+	origin.StartAt = time.Unix(req.StartAt, 0)
+	origin.EndAt = time.Unix(req.EndAt, 0)
 	origin.StartTime = req.StartTime
 	origin.EndTime = req.EndTime
 	origin.Regex = req.Regex
