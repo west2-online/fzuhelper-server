d81b0842b21bbc0d871f8097641bb69a7392a537
FantasyRL <1845983502@qq.com>
2024-12-30 19:25:27 +0800

    test: add test in db

--- internal/user/service/get_logindata_test.go
+++ internal/user/service/get_logindata_test.go
@@ -15,3 +15,66 @@ limitations under the License.
 */
 
 package service
+
+/*
+func TestUserService_GetLoginData(t *testing.T) {
+	type testCase struct {
+		name           string
+		expectedId     string
+		expectedCookie []*http.Cookie
+		mockError      error
+		expectingError bool
+	}
+	testCases := []testCase{
+		{
+			name:       "success",
+			expectedId: "2024102301000",
+			expectedCookie: []*http.Cookie{
+				&http.Cookie{
+					Name: "test",
+				},
+			},
+		},
+		{
+			name:           "jwch error",
+			mockError:      errno.InternalServiceError,
+			expectingError: true,
+		},
+	}
+	req := &user.GetLoginDataRequest{
+		Id:       "102301000",
+		Password: "102301000",
+	}
+	defer mockey.UnPatchAll()
+	for _, tc := range testCases {
+		mockey.PatchConvey(tc.name, t, func() {
+			mockClientSet := new(base.ClientSet)
+			mockClientSet.SFClient = new(utils.Snowflake)
+			mockClientSet.DBClient = new(db.Database)
+			userService := NewUserService(context.Background(), "", nil, mockClientSet)
+			mockey.Mock((*jwch.Student).GetIdentifierAndCookies).To(func() (string, []*http.Cookie, error) {
+				if tc.expectingError {
+					return "", nil, tc.mockError
+				}
+				return tc.expectedId, tc.expectedCookie, nil
+			}).Build()
+			mockey.Mock((*UserService).insertStudentInfo).IncludeCurrentGoRoutine().To(func(req *user.GetLoginDataRequest, stu *jwch.Student) { return }).Build()
+
+			mockey.Mock((*userdb.DBUser).GetStudentById).To(func(ctx context.Context, stuId string) (bool, *model.Student, error) {
+				return true, nil, nil
+			}).Build()
+
+			id, cookie, err := userService.GetLoginData(req)
+			if tc.expectingError {
+				assert.Nil(t, cookie)
+				assert.Contains(t, err.Error(), errno.InternalServiceError.ErrorMsg)
+			} else {
+				assert.NoError(t, err)
+				assert.Equal(t, tc.expectedId, id)
+				assert.Equal(t, utils.ParseCookiesToString(tc.expectedCookie), cookie)
+			}
+		})
+	}
+}
+
+*/
