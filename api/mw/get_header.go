1442ee51286cd55f36df6304ee9dbabd67ea95ed
jiuxia211 <2064166368@qq.com>
2025-01-20 14:06:33 +0800

    refactor: clean unnecesssary ctx

--- api/mw/get_header.go
+++ api/mw/get_header.go
@@ -24,7 +24,6 @@ import (
 	"github.com/west2-online/fzuhelper-server/api/pack"
 	"github.com/west2-online/fzuhelper-server/kitex_gen/model"
 	metainfoContext "github.com/west2-online/fzuhelper-server/pkg/base/context"
-	"github.com/west2-online/fzuhelper-server/pkg/base/login_data"
 	"github.com/west2-online/fzuhelper-server/pkg/errno"
 )
 
@@ -38,10 +37,6 @@ func GetHeaderParams() app.HandlerFunc {
 			c.Abort()
 			return
 		}
-		ctx = login_data.NewContext(ctx, &model.LoginData{
-			Id:      id,
-			Cookies: cookies,
-		})
 
 		// 实现规范化服务透传，不需要中间进行编解码
 		ctx = metainfoContext.WithLoginData(ctx, &model.LoginData{
