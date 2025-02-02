f5856803857ffcc4db19aa99f57c57934ecfbd62
FantasyRL <1845983502@qq.com>
2025-02-02 16:50:34 +0800

    fix: unit test

--- internal/user/service/get_info_test.go
+++ internal/user/service/get_info_test.go
@@ -28,9 +28,7 @@ import (
 	"github.com/stretchr/testify/assert"
 	"gorm.io/gorm"
 
-	"github.com/west2-online/fzuhelper-server/kitex_gen/model"
 	"github.com/west2-online/fzuhelper-server/pkg/base"
-	meta "github.com/west2-online/fzuhelper-server/pkg/base/context"
 	"github.com/west2-online/fzuhelper-server/pkg/cache"
 	"github.com/west2-online/fzuhelper-server/pkg/cache/user"
 	"github.com/west2-online/fzuhelper-server/pkg/db"
@@ -42,120 +40,6 @@ import (
 )
 
 func TestUserService_GetUserInfo(t *testing.T) {
-	type testCase struct {
-		name              string
-		expectedInfo      *dbmodel.Student
-		expectedJwch      *jwch.StudentDetail
-		expectedExist     bool
-		mockError         error
-		mockDBCreateError error
-		mockJwchError     error
-		expectingError    bool
-		expectingErrorMsg string
-	}
-	info := &dbmodel.Student{
-		StuId:    "102301000",
-		Sex:      "sex",
-		Birthday: "1970-01-01",
-		College:  "计算机与大数据学院",
-		Grade:    2023,
-		Major:    "计算机类",
-	}
-	stuDetail := &jwch.StudentDetail{
-		Sex:      info.Sex,
-		Birthday: info.Birthday,
-		College:  info.College,
-		Grade:    strconv.FormatInt(info.Grade, 10),
-		Major:    info.Major,
-	}
-	testCases := []testCase{
-		{
-			name:           "exist success",
-			expectedExist:  true,
-			expectingError: false,
-			expectedInfo:   info,
-			mockError:      nil,
-		},
-		{
-			name:           "not exist success",
-			expectedExist:  false,
-			expectingError: false,
-			expectedInfo:   info,
-			expectedJwch:   stuDetail,
-			mockError:      nil,
-		},
-		{
-			name:              "jwch error",
-			expectedExist:     false,
-			expectingError:    true,
-			expectedInfo:      info,
-			expectedJwch:      stuDetail,
-			mockJwchError:     errno.InternalServiceError,
-			expectingErrorMsg: errno.InternalServiceError.ErrorMsg,
-		},
-		{
-			name:              "db create error",
-			expectedExist:     false,
-			expectingError:    true,
-			expectedInfo:      info,
-			expectedJwch:      stuDetail,
-			mockDBCreateError: gorm.ErrInvalidData,
-			expectingErrorMsg: "service.GetUserInfo:",
-		},
-		{
-			name:              "db error",
-			expectedExist:     false,
-			expectingError:    true,
-			expectedInfo:      info,
-			expectedJwch:      stuDetail,
-			mockError:         gorm.ErrInvalidData,
-			expectingErrorMsg: "service.GetUserInfo:",
-		},
-	}
-	defer mockey.UnPatchAll()
-	for _, tc := range testCases {
-		mockey.PatchConvey(tc.name, t, func() {
-			mockClientSet := new(base.ClientSet)
-			mockClientSet.SFClient = new(utils.Snowflake)
-			mockClientSet.DBClient = new(db.Database)
-			mockClientSet.CacheClient = new(cache.Cache)
-			userService := NewUserService(context.Background(), "", nil, mockClientSet)
-
-			mockey.Mock((*cache.Cache).IsKeyExist).Return(false).Build()
-			mockey.Mock((*user.CacheUser).SetStuInfoCache).Return(nil).Build()
-			mockey.Mock((*user.CacheUser).GetStuInfoCache).Return(nil).Build()
-			mockey.Mock((*userDB.DBUser).GetStudentById).To(func(ctx context.Context, stuId string) (bool, *dbmodel.Student, error) {
-				return tc.expectedExist, tc.expectedInfo, tc.mockError
-			}).Build()
-			mockey.Mock((*jwch.Student).WithLoginData).To(func(identifier string, cookies []*http.Cookie) *jwch.Student {
-				return jwch.NewStudent()
-			}).Build()
-			mockey.Mock(meta.GetLoginData).To(func(ctx context.Context) (*model.LoginData, error) {
-				return &model.LoginData{
-					Id:      "1111111111111111111111111111111111",
-					Cookies: "",
-				}, nil
-			}).Build()
-			mockey.Mock((*jwch.Student).GetInfo).To(func() (resp *jwch.StudentDetail, err error) {
-				return tc.expectedJwch, tc.mockJwchError
-			}).Build()
-			mockey.Mock((*userDB.DBUser).CreateStudent).To(func(ctx context.Context, userModel *dbmodel.Student) error {
-				return tc.mockDBCreateError
-			}).Build()
-
-			stuInfo, err := userService.GetUserInfo(info.StuId)
-			if tc.expectingError {
-				assert.Nil(t, stuInfo)
-				assert.Contains(t, err.Error(), tc.expectingErrorMsg)
-			} else {
-				assert.NoError(t, err)
-				assert.Equal(t, tc.expectedInfo, stuInfo)
-			}
-		})
-	}
-}
-
-func TestUserService2_GetUserInfo(t *testing.T) {
 	// 测试案例使用的结构体
 	type testCase struct {
 		name string
