7165f8e59dfdc498ae497c69cedbef896b297ea5
jiuxia211 <2064166368@qq.com>
2025-03-04 23:06:52 +0800

    fix: exclude bot in contributor info

--- internal/common/pack/contributor.go
+++ internal/common/pack/contributor.go
@@ -21,14 +21,19 @@ import (
 	"github.com/west2-online/fzuhelper-server/pkg/github"
 )
 
+import "strings"
+
 func BuildContributors(data []*github.Contributor) []*model.Contributor {
-	contributors := make([]*model.Contributor, len(data))
+	var contributors []*model.Contributor
+
 	for i := range data {
-		contributors[i] = &model.Contributor{
-			Name:          data[i].Login,
-			AvatarUrl:     data[i].AvatarUrl,
-			Url:           data[i].HtmlUrl,
-			Contributions: data[i].Contributions,
+		if !strings.Contains(data[i].Login, "dependabot") {
+			contributors = append(contributors, &model.Contributor{
+				Name:          data[i].Login,
+				AvatarUrl:     data[i].AvatarUrl,
+				Url:           data[i].HtmlUrl,
+				Contributions: data[i].Contributions,
+			})
 		}
 	}
 
