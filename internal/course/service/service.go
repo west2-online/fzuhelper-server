e9d87504670c7369e523a0bd15c4c183cf5f52d1
SchwarzSail <119314702+SchwarzSail@users.noreply.github.com>
2025-03-24 10:59:54 +0800

    feat: adding common rpc (#257)
    * adding common rpc
    
    * fix
    
    * fix getLatestStartTerm
    
    * adding taskqueue inject
    
    ---------
    
    Co-authored-by: Baoshuo <i@baoshuo.ren>

--- internal/course/service/service.go
+++ internal/course/service/service.go
@@ -19,6 +19,7 @@ package service
 import (
 	"context"
 
+	"github.com/west2-online/fzuhelper-server/kitex_gen/common/commonservice"
 	"github.com/west2-online/fzuhelper-server/pkg/base"
 	"github.com/west2-online/fzuhelper-server/pkg/cache"
 	"github.com/west2-online/fzuhelper-server/pkg/db"
@@ -27,19 +28,21 @@ import (
 )
 
 type CourseService struct {
-	ctx       context.Context
-	db        *db.Database
-	sf        *utils.Snowflake
-	cache     *cache.Cache
-	taskQueue taskqueue.TaskQueue
+	ctx          context.Context
+	db           *db.Database
+	sf           *utils.Snowflake
+	cache        *cache.Cache
+	taskQueue    taskqueue.TaskQueue
+	commonClient commonservice.Client
 }
 
 func NewCourseService(ctx context.Context, clientset *base.ClientSet, taskQueue taskqueue.TaskQueue) *CourseService {
 	return &CourseService{
-		ctx:       ctx,
-		db:        clientset.DBClient,
-		sf:        clientset.SFClient,
-		cache:     clientset.CacheClient,
-		taskQueue: taskQueue,
+		ctx:          ctx,
+		db:           clientset.DBClient,
+		sf:           clientset.SFClient,
+		cache:        clientset.CacheClient,
+		taskQueue:    taskQueue,
+		commonClient: clientset.CommonClient,
 	}
 }
