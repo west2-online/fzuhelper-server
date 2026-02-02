8177a4036177b4aafa5a62acd63eef6477312f46
Lin Rongda <102654433+linrongda@users.noreply.github.com>
2026-02-02 22:49:36 +0800

    fix: 增加umeng推送延时，修复限流异常 (#398)

--- pkg/constants/umeng.go
+++ pkg/constants/umeng.go
@@ -21,7 +21,7 @@ import "time"
 const (
 	UmengURL               = "https://msgapi.umeng.com/api/send" // 推送 API
 	UmengMessageExpireTime = 3 * ONE_DAY                         // 推送消息过期时间
-	UmengRateLimitDelay    = 5 * time.Second                     // 用于在成绩 diff 循环中等待，防止被友盟限流
+	UmengRateLimitDelay    = 30 * time.Second                    // 用于在发送通知循环中等待，防止被友盟限流
 )
 
 // Tag
