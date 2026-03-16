bd4d94dda8e6627554e3cb509fb8ace94b9a86e6
Cai <13110818005@qq.com>
2026-03-16 23:54:00 +0800

    test(utils): 添加GetWeekdayByDate测试

---
+++ pkg/utils/utils_test.go
@@ -0,0 +1,70 @@
+/*
+Copyright 2024 The west2-online Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package utils
+
+import "testing"
+
+func TestGetWeekdayByDate(t *testing.T) {
+	tests := []struct {
+		name          string
+		termStartDate string
+		date          string
+		wantWeek      int
+		wantWeekday   int
+		wantErr       bool
+	}{
+		{
+			name:          "Normal",
+			termStartDate: "2026-03-02",
+			date:          "2026-03-17",
+			wantWeek:      3,
+			wantWeekday:   2,
+			wantErr:       false,
+		},
+		{
+			name:          "StartDateIsNotMonday",
+			termStartDate: "2026-03-08",
+			date:          "2026-03-17",
+			wantWeek:      3,
+			wantWeekday:   2,
+			wantErr:       false,
+		},
+		{
+			name:          "DateBeforeTermStart",
+			termStartDate: "2024-03-05",
+			date:          "2024-03-01",
+			wantWeekday:   0,
+			wantWeek:      0,
+			wantErr:       true,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			week, weekday, err := GetWeekdayByDate(tt.termStartDate, tt.date)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("GetWeekdayByDate() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if week != tt.wantWeek {
+				t.Errorf("GetWeekdayByDate() week = %v, want %v", week, tt.wantWeek)
+			}
+			if weekday != tt.wantWeekday {
+				t.Errorf("GetWeekdayByDate() weekday = %v, want %v", weekday, tt.wantWeekday)
+			}
+		})
+	}
+}
