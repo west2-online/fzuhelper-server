f3eb7081567cd6dc77f635251861bf1c612a7df1
yang <143949502+1341292919@users.noreply.github.com>
2026-03-18 10:47:04 +0800

    fix(course): write term list to database (#433)

--- internal/course/service/get_course_list.go
+++ internal/course/service/get_course_list.go
@@ -101,6 +101,10 @@ func (s *CourseService) GetCourseList(req *course.CourseListRequest, loginData *
 	s.taskQueue.Add(fmt.Sprintf("putCourse:%s", stuId), taskqueue.QueueTask{Execute: func() error {
 		return s.putCourseToDatabase(stuId, req.Term, pack.BuildCourse(courses))
 	}})
+	// 学期列表异步存库
+	s.taskQueue.Add(fmt.Sprintf("putTerms:%s", stuId), taskqueue.QueueTask{Execute: func() error {
+		return s.putTermToDatabase(stuId, pack.BuildTermOnDB(terms.Terms))
+	}})
 
 	return s.removeDuplicateCourses(pack.BuildCourse(courses)), nil
 }
@@ -209,6 +213,10 @@ func (s *CourseService) GetCourseListYjsy(req *course.CourseListRequest, loginDa
 	// 异步将课程列表存入数据库
 	s.taskQueue.Add(fmt.Sprintf("putCourse:%s", stuId), taskqueue.QueueTask{Execute: func() error {
 		return s.putCourseToDatabase(stuId, req.Term, pack.BuildCourseYjsy(courses))
+	}})
+	// 学期列表异步存库
+	s.taskQueue.Add(fmt.Sprintf("putTerms:%s", stuId), taskqueue.QueueTask{Execute: func() error {
+		return s.putTermToDatabase(stuId, pack.BuildTermOnDB(terms.Terms))
 	}})
 
 	return pack.BuildCourseYjsy(courses), nil
