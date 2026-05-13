574d52c38ed590836a32bbd3bee5960c4f747b2a
155TuT <zhiyiwang155tut@gmail.com>
2026-05-13 23:04:42 +0800

    feat(toolbox): add ConfigID and StudentID

--- api/pack/common.go
+++ api/pack/common.go
@@ -79,15 +79,17 @@ func BuildContributors(contributors []*model.Contributor) []*api.Contributor {
 
 func BuildToolboxConfig(config *model.ToolboxConfig) *api.ToolboxConfig {
 	return &api.ToolboxConfig{
-		ToolID:   config.ToolId,
-		Visible:  config.Visible,
-		Name:     config.Name,
-		Icon:     config.Icon,
-		Type:     config.Type,
-		Message:  config.Message,
-		Extra:    config.Extra,
-		Platform: config.Platform,
-		Version:  config.Version,
+		ToolID:    config.ToolId,
+		Visible:   config.Visible,
+		Name:      config.Name,
+		Icon:      config.Icon,
+		Type:      config.Type,
+		Message:   config.Message,
+		Extra:     config.Extra,
+		Platform:  config.Platform,
+		Version:   config.Version,
+		ConfigID:  config.ConfigId,
+		StudentID: config.StudentId,
 	}
 }
 
